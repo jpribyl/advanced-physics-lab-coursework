ab8ee5e5e3c7885ef5764e3996e138d5ede79244
jpribyl <pribylsnbits@gmail.com>
2018-04-20 16:06:12 -0600

    added HO fit

--- lab4/src/analysis/model.py
+++ lab4/src/analysis/model.py
@@ -1,8 +1,9 @@
-from scipy.fftpack import fft
-
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
+from scipy import signal
+from scipy.fftpack import fft
+from scipy.optimize import curve_fit
 
 class model(object):
 
@@ -45,7 +46,7 @@ class fourierModel(model):
     """
 
     def __init__(self,
-                 y,
+                 y=None,
                  numPoints=10000,
                  sampleSpacing=(1/250),
                  name='Fourier Model'):
@@ -101,3 +102,198 @@ class fourierModel(model):
         )
 
         return
+
+
+class lrc(fourierModel):
+
+    """
+    The LRC circuit is a fourier model
+    """
+
+    def __init__(self,
+                 C=10 * 10**-9,
+                 R=1000,
+                 L=68 * 10**-3,
+                 import_phase=True,
+                 numPoints=6000,
+                 sampleSpacing=1/800,
+                 name='LRC-Model'):
+
+        """
+        LRC's need values for L, R, and C as well as frequencies measured
+        we can set this model's name automatically
+        """
+        # setting the model.name attribute
+        super(lrc, self).__init__(
+            numPoints=numPoints,
+            sampleSpacing=sampleSpacing,
+            name=name)
+
+        self.C = C
+        self.R = R
+        self.L = L
+        self.import_phase = import_phase
+
+
+    def voltageOut(self):
+        """
+        :returns: Voltage out in frequency domain calculated by the transfer
+        function multiplied by voltage in (IE self.transformY())
+        """
+
+        return (1 / (np.sqrt(
+        (1 - 4 * self.C * self.transformX()**2 * self.L * np.pi**2)**2 +
+        (2 * self.C * self.transformX() * np.pi * self.R)**2))) * self.transformY()
+
+    def phaseOut(self,
+                    path='/home/johnny/kod/py/bin/venv/py3/phx444/labs/lab3/data/lrc_phase_df.csv',
+                    plot=False):
+
+        # if self.import_phase:
+            # source_list = pd.read_csv(path)
+            # self.phaseFreq = source_list['frequency']
+            # self.phase = source_list['phase']
+
+        # else:
+
+        return - np.arctan2(
+        (2 * self.C * self.transformX() * np.pi * self.R),
+        (1 - 4 * self.C * self.transformX()**2 * self.L * np.pi**2))
+
+    def model(self, style='--'):
+        """
+        :returns: Nothing, but will plot the FT of the model
+
+        plt.plot(
+                    self.transformX(),
+                    self.voltageOut(),
+                    style,
+                    label=self.name
+                )
+
+
+        """
+        plt.plot(
+            self.transformX(),
+            self.voltageOut(),
+            style,
+            label=self.name
+        )
+
+class lrcMultiFreq(lrc):
+
+    """
+    LRC model for multiple frequenquency noise / chirp inputs
+    """
+
+    def __init__(
+        self,
+        numPoints=6000,
+        sampleSpacing=1/30000,
+        xvals=None,
+        yvals=None,
+        yerr=None,
+        amplitude=2.5):
+        """
+        :freq: frequency of the square input
+        """
+        lrc.__init__(
+            self,
+            numPoints=numPoints,
+            sampleSpacing=sampleSpacing)
+
+        self.voltageFunction = amplitude * np.random.rand(len(self.time))
+        self.xvals = xvals
+        self.yvals = yvals
+        self.yerr = yerr
+
+
+
+
+class lrcMultiFreqGain(lrcMultiFreq):
+
+    """
+    Class that will model the magnitude of the gain for chirps / noise through
+    the LRC circuit. 
+
+    :xvals: The corresponding xanvalues from an instance of the measurement
+    class. This data (Pandas DataFrame) will be used to fit parameters to the
+    gain function
+
+    :yvals: corresponding measured yanvalues
+
+    :yerr: corresponding measured yanerror
+    """
+
+    def __init__(
+        self,
+        xvals,
+        yvals,
+        yerr,
+        numPoints=10000,
+        sampleSpacing=1/250,
+        amplitude=2.5):
+
+        """
+        Call the superclass initializaion
+        """
+        lrcMultiFreq.__init__(
+            self,
+            numPoints,
+            sampleSpacing,
+            xvals,
+            yvals,
+            yerr,
+            amplitude)
+
+
+
+    def lrcGainFunction(self, x, amp, m, w0, xi):
+        """
+        This is the magnitude of the gain function for the LRC - I produced it
+        using Mathematica's ComplexExpand[] and Abs[]
+
+        :x: frequency (X-axis)
+        :amp: amplitude parameter of Gain
+        :L: induction of LRC
+        :R: resistance of LRC
+        :C: capacitance of LRC
+
+        :returns: a pandas DF or number depending upon data type of the input
+        """
+
+        return amp / (m * x * np.sqrt(
+            (2*w0*xi)**2 + (w0**2 - x**2)**2 / x**2
+            
+            ))
+
+
+    def model(self, style='--', p0=[5, 10**-13, 50, 1], maxfev=1000):
+        """Model method override
+
+        :style: the line style for this plot
+
+        :returns: Nothing, but adds the magnitude of the gain to the current
+        plot
+
+        """
+
+        x = self.xvals
+        y = self.yvals
+        p0 = np.array(p0)
+        yerr = self.yerr
+
+        popt, pcov = curve_fit(
+            self.lrcGainFunction,
+            x,
+            y,
+            sigma=yerr,
+            maxfev=maxfev,
+            p0=p0,
+        )
+
+        xfit = x
+        yfit = self.lrcGainFunction(x, *popt)
+
+        # plt.plot(xfit, yfit, style, label='LRC Gain Function Model')
+        return xfit, yfit
